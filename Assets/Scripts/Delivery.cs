dbc46b7892188848909bd5e94636db33543f3dc8
Phoenixmorrisville <mamrak387@morrisville.edu>
2025-09-25 21:38:48 -0400

    Video 05: Using Bool

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Package"))
         {
             Debug.Log("Pick up Package");
+            hasPackage = true;
         }
-        if (collision.CompareTag("Customer"))
+        if (collision.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered Package");
+            hasPackage = false;
         }
 
     }
