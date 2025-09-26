2e0e52e3000472d0a2c3daa7c5a6466739044781
Phoenixmorrisville <mamrak387@morrisville.edu>
2025-09-25 21:51:06 -0400

    Video 07: Destroy Object

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float delay = 1f;
     bool hasPackage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package"))
+        if (collision.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Pick up Package");
             hasPackage = true;
+            Destroy(collision.gameObject, delay);
         }
         if (collision.CompareTag("Customer") && hasPackage)
         {
