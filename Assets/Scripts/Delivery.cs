01185906d92f8269b3405747b5bd5e67cc36d415
Phoenixmorrisville <mamrak387@morrisville.edu>
2025-09-27 08:52:31 -0400

    Added storage system to hold more than one package

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,25 +1,43 @@
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class Delivery : MonoBehaviour
 {
     [SerializeField] float delay = 1f;
+
     bool hasPackage;
+    int package = 0;
+    public TextMeshProUGUI StorageText;
+    private void Start()
+    {
+        StorageText.gameObject.SetActive(true);
+        StorageText.text = "Storage: "+ package.ToString() + "/3";
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package") && !hasPackage)
+        if (collision.CompareTag("Package") && package <= 2)
         {
             Debug.Log("Pick up Package");
             hasPackage = true;
             GetComponent<ParticleSystem>().Play();
+            package++;
             Destroy(collision.gameObject, delay);
         }
         if (collision.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered Package");
-            hasPackage = false;
-            GetComponent<ParticleSystem>().Stop();
+            package --;
+            if (package == 0)
+            {
+                hasPackage = false;
+                GetComponent<ParticleSystem>().Stop();
+            }
             Destroy(collision.gameObject, delay);
         }
 
+    }
+    private void Update()
+    {
+        StorageText.text = "Storage: " + package.ToString() + "/3";
     }
 }
